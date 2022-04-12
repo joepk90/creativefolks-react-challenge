0aa41c90d8022f3f550d2e1235f7f97b62e91f7d
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-12 21:57:10 +0100

    setup toggle to show or hide comments in the PostCard component. import the comments context to print the amount of comments above each comment list

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,13 +1,50 @@
+import { useState, useContext } from 'react';
 import CommentList from "components/CommentList";
+import { Context } from 'contexts/Context';
+import { filterComments } from 'lib/utilities';
 
 const PostCard = ({ post }) => {
+
+    const { comments } = useContext(Context)
+
+    const [showComments, setShowComments] = useState(false)
+
+    const postComments = filterComments(post.id, comments)
+
+    const renderToggleCommentsLink = () => {
+
+        let hideCommentText = 'Hide Comments';
+        let showCommentText = 'Show Comments';
+        const showHideCommentsText = showComments ? hideCommentText : showCommentText;
+
+        return (
+            <a
+                role="button"
+                className='small d-block mb-3'
+                onClick={() => setShowComments(!showComments)}
+            >{showHideCommentsText} ({postComments.length})</a>
+        );
+
+    }
+
+    const renderComments = () => {
+
+        if (showComments === false) return (<></>);
+
+        return (
+            <>
+                <CommentList postId={post.id} />
+            </>
+        )
+    }
 
     return (
         <div className="post-card">
             <h2 >{post.title}</h2>
             <p>The post id is {post.id}</p>
 
-            <CommentList postId={post.id} />
+            {renderToggleCommentsLink()}
+            {renderComments()}
 
         </div>
     );
